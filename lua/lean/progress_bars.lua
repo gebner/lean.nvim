7da964d738dedfde479f9ab2d05af955ebc2d6bb
Gabriel Ebner <gebner@gebner.org>
2021-08-16 18:39:58 +0200

    Add types and tweaks to progress_bars.

--- lua/lean/progress_bars.lua
+++ lua/lean/progress_bars.lua
@@ -1,15 +1,37 @@
-local M = {}
-local options = { _DEFAULTS = { priority = 10, character = '⋯' } }
+local progress_bars = {}
+local options = { _DEFAULTS = { priority = 10, character = '┋' } }
 
 local progress_sign = 'leanSignProgress'
 local sign_ns = 'leanSignProgress'
 
--- Table from bufnr to current processing info.
-M.proc_infos = {}
+---@class LspPosition
+---@field line integer
+---@field character integer
 
+---@class LspRange
+---@field start LspPosition
+---@field end LspPosition
+
+---@class LeanFileProgressProcessingInfo
+---@field range LspRange
+
+---@class VersionedTextDocumentIdentifier
+---@field uri string
+---@field version integer
+
+---@class LeanFileProgressParams
+---@field textDocument VersionedTextDocumentIdentifier
+---@field processing LeanFileProgressProcessingInfo[]
+
+--- Table from bufnr to current processing info.
+---@type table<number, LeanFileProgressProcessingInfo[]>
+local proc_infos = {}
+
+--- Updates the signs according to the ranges in proc_infos.
+---@param bufnr number
 local function update(bufnr)
   vim.fn.sign_unplace(sign_ns, { buffer = bufnr })
-  for _, proc_info in ipairs(M.proc_infos[bufnr]) do
+  for _, proc_info in ipairs(proc_infos[bufnr]) do
     local start_line = proc_info.range.start.line + 1
     local end_line = proc_info.range['end'].line + 1
     for line = start_line, end_line do
@@ -21,13 +43,15 @@ local function update(bufnr)
   end
 end
 
--- Table from bufnr to timer object.
+--- Table from bufnr to timer object.
+---@type table<number, any>
 local timers = {}
 
+---@param params LeanFileProgressParams
 local function on_file_progress(err, _, params, _, _, _)
   if err ~= nil then return end
   local bufnr = vim.uri_to_bufnr(params.textDocument.uri)
-  M.proc_infos[bufnr] = params.processing
+  proc_infos[bufnr] = params.processing
   if timers[bufnr] == nil then
     timers[bufnr] = vim.defer_fn(function ()
       timers[bufnr] = nil
@@ -36,7 +60,7 @@ local function on_file_progress(err, _, params, _, _, _)
   end
 end
 
-function M.enable(opts)
+function progress_bars.enable(opts)
   options = vim.tbl_extend("force", options._DEFAULTS, opts)
   vim.fn.sign_define(progress_sign, {
     text = options.character,
@@ -46,4 +70,4 @@ function M.enable(opts)
   vim.lsp.handlers['$/lean/fileProgress'] = on_file_progress
 end
 
-return M
+return progress_bars
